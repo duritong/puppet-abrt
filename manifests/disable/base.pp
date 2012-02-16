107aa3e152de4a8bb9d5e9f53aa76933a01e19bb
mh <mh@immerda.ch>
2012-02-16 18:06:39 +0100

    correctly manage all deps

--- manifests/disable/base.pp
+++ manifests/disable/base.pp
@@ -1,8 +1,15 @@
 class abrt::disable::base inherits abrt::base {
+  package{
+    'abrt-cli':
+      ensure => absent;
+    ['abrt-addon-kerneloops','abrt-addon-ccpp','abrt-addon-python']:
+      require => Package['abrt-cli'],
+      ensure => absent;
+  }
 
   Package['abrt']{
     ensure => absent,
-    require => Service['abrtd','abrt-oops','abrt-ccpp'],
+    require => [Service['abrtd','abrt-oops','abrt-ccpp'], Package['abrt-addon-kerneloops','abrt-addon-ccpp','abrt-addon-python']],
   }
 
   Service['abrtd','abrt-oops','abrt-ccpp']{
